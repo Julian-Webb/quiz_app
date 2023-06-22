93f04ad1f7a183d5cca32a231c8c09e6fcbc2057
Julian-Webb <julian.webb00@gmail.com>
2023-06-22 21:25:59 +0200

    Added question screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    // MaterialApp is for apps that use Material Design (google's design system)
-    const MaterialApp(
-      title: 'Quiz App',
-      home: Scaffold(
-        body: GradientContainer(
-          [Color.fromARGB(255, 97, 24, 110), Color.fromARGB(255, 60, 16, 68)],
-        ),
-      ),
-    ),
-  );
+  // runApp tells flutter which widget to display on the screen
+  runApp(const Quiz());
 }
